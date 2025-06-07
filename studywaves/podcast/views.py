d7ae06cd677bfc94cd275f8c8cd857e30d6dc622
Aditya Chandrashekaran <adityacrao19@gmail.com>
2025-06-07 09:55:49 -0700

    test

--- studywaves/podcast/views.py
+++ studywaves/podcast/views.py
@@ -16,8 +16,7 @@ def index(request):
         token = user_social.extra_data['access_token'],
         refresh_token = user_social.extra_data.get('refresh_token'),
         token_uri = 'https://oauth2.googleapis.com/token',
-        client_id = '620487105253-e7ljd0mq6684qhl29qsdj331caer8qlg.apps.googleusercontent.com', # put client ID
-        client_secret = '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', #put secret 
+        
         scopes = ['https://www.googleapis.com/auth/drive.readonly'],
     )
 
