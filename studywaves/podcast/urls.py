a05fbfc38db536e7d9702cc1d728ccee8c3960bf
Aditya Chandrashekaran <adityacrao19@gmail.com>
2025-06-08 11:21:02 -0700

    final

--- studywaves/podcast/urls.py
+++ studywaves/podcast/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('get-file-content/<str:file_id>/', views.get_file_content, name='get_file_content'),
     path('audio-player/<int:podcast_id>/', views.audio_player, name='audio_player'),
     path('api/ask-question/', views.ask_question, name='ask_question'),
     path('create-podcast/', views.create_podcast, name='create_podcast'),
